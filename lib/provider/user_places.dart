e5ff8c4f010c3e25f6bcf3d56b4c9fec3e13e18c
Yogesh Raut <hiyogesh.raut@gmail.com>
2025-02-08 18:00:23 +0530

    Storing place data in local database, in device.

--- lib/provider/user_places.dart
+++ lib/provider/user_places.dart
@@ -1,6 +1,8 @@
 import 'package:riverpod/riverpod.dart';
 import 'package:path_provider/path_provider.dart' as syspath;
 import 'package:path/path.dart' as path;
+import 'package:sqflite/sqflite.dart' as sql;
+import 'package:sqflite/sqlite_api.dart';
 
 import 'package:favourite_places/models/place.dart';
 
@@ -12,6 +14,25 @@ class UserPlacesNotifier extends StateNotifier<List<Place>> {
     final fileName = path.basename(place.selectedImage.path);
     final savedImage =
         await place.selectedImage.copy('${appDir.path}/$fileName');
+    final dbPath = await sql.getDatabasesPath();
+    final db = await sql.openDatabase(
+      path.join(dbPath, 'places.db'),
+      onCreate: (db, version) {
+        return db.execute(
+            'CREATE TABLE user_places(id TEXT PRIMARY KEY, title TEXT, image TEXT, loc_lat REAL, loc_lng REAL, address TEXT)');
+      },
+      version: 1,
+    );
+
+    db.insert('user_places', {
+      'id': place.id,
+      'title': place.title,
+      'image': savedImage.path,
+      'loc_lat': place.location.latitude,
+      'loc_lng': place.location.longitude,
+      'address': place.location.address,
+    });
+
     final newPlace = Place(
       title: place.title,
       location: place.location,
